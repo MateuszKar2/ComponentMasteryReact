74560eb15fe7971d59ed60daf8fe8237fb00c96b
MateuszKar2 <system505@protonmail.com>
2023-08-31 07:20:22 +0200

    add file

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export const Profile  = ({username, tag, location, avatar, stats}) => {
 return (
@@ -29,4 +31,16 @@ return (
   </ul>
 </div>
 )
+}
+
+PropTypes.Profile = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  })
 }
