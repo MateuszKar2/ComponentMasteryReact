acb1b5263be0ccc87db2a01fff0f1baab8720ac0
MateuszKar2 <system505@protonmail.com>
2023-08-31 12:35:52 +0200

    save changes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,38 +2,22 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 import { getRandomHexColor } from 'utils/getRandomHexColor';
 
-export const Statistics = ({id, title, stats}) => {
+export const Statistics = ({title, stats}) => {
   return  (
     <section className="statistics">
-    <h2 className="title">Upload stats</h2>
+    <h2 className="title">{title}</h2>
     <ul className="stat-list">
     {
-  id.map(el => {
+  stats.map(el => {
     const colorList = { backgroundColor: getRandomHexColor() };
     return (
       <li className="item" style={colorList} key={el.id}>
-      <span className="label">{title}</span>
-      <span className="percentage">{stats}</span>
+      <span className="label">{el.label}</span>
+      <span className="percentage">{el.percentage}%</span>
     </li>
     );
   })
 }
-    {/* <li className="item">
-      <span className="label">{title}</span>
-      <span className="percentage">{stats}</span>
-    </li>
-    <li className="item">
-      <span className="label">{title}</span>
-      <span className="percentage">{stats}</span>
-    </li>
-    <li className="item">
-      <span className="label">{title}</span>
-      <span className="percentage">{stats}</span>
-    </li>
-    <li className="item">
-      <span className="label">{title}</span>
-      <span className="percentage">{stats}</span>
-    </li> */}
   </ul>
 </section>
     )
