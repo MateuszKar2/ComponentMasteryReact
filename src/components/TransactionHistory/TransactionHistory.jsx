13fd7909e0ca4c50f0334ea8e226edd046ecce2c
MateuszKar2 <system505@protonmail.com>
2023-08-31 07:20:39 +0200

    add file

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({items}) => {
+    return (
+        <table className="transaction-history">
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <td>Invoice</td>
+      <td>125</td>
+      <td>USD</td>
+    </tr>
+    <tr>
+      <td>Withdrawal</td>
+      <td>85</td>
+      <td>USD</td>
+    </tr>
+  </tbody>
+</table>
+    )
+}
+
+PropTypes.TransactionHistory = {
+    items: PropTypes.arrayOf()
+}
